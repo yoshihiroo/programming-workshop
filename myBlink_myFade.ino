aa8c9ac2f0c6756b3f32bd6a0ee3048a69b13feb
yoshihiroo <yoshihiroo@hotmail.com>
2016-11-19 16:13:11 +0900

    Create myBlink_myFade.ino

---
+++ myBlink_myFade.ino
@@ -0,0 +1,32 @@
+
+void setup() {
+  pinMode(9, OUTPUT);
+}
+
+void loop() {
+  myBlink();
+  myFade();
+}
+
+void myBlink() {
+  digitalWrite(9, HIGH);
+  delay(500);
+  digitalWrite(9, LOW);
+  delay(500);
+}
+
+void myFade() {
+  int i = 0;
+  while (i < 256) {
+    analogWrite(9, i);
+    i = i + 5;
+    delay(10);
+  }
+  i = 255;
+  while (i > 0) {
+    analogWrite(9, i);
+    i = i - 5;
+    delay(10);
+  }
+
+}
