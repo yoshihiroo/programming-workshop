a7c8ea4a3d0fd57395931a4c763e3bbed239e59e
yoshihiroo <yoshihiroo@hotmail.com>
2016-11-20 07:44:22 +0900

    Update analogWriteSample.ino

--- Arduino/src/analogWriteSample.ino
+++ Arduino/src/analogWriteSample.ino
@@ -2,16 +2,16 @@ void setup() {
 }
 
 void loop() {
-  analogWrite(9, 255);  //0~255
+  analogWrite(9, 255);  // 0~255
+  delay(1000);          // 1,000 millisecond
+
+  analogWrite(9, 150);
   delay(1000);
 
-  analogWrite(9, 150);  //0~255
+  analogWrite(9, 50);
   delay(1000);
 
-  analogWrite(9, 50);  //0~255
-  delay(1000);
-
-  analogWrite(9, 20);  //0~255
+  analogWrite(9, 20);
   delay(1000);
 
 }
