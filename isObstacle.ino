b3ab9d8b5315befa79be6cbebe5a20d3d17dda60
yoshihiroo <yoshihiroo@hotmail.com>
2016-11-19 16:23:57 +0900

    Update isObstacle.ino

--- isObstacle.ino
+++ isObstacle.ino
@@ -1,42 +1,16 @@
 void setup() {
-  pinMode(9, OUTPUT);
   pinMode(7, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   if (digitalRead(7) == LOW) // is Obstacle
   {
-    myBlink(300);
+    Serial.println("OBSTACLE!!, OBSTACLE!!");
   }
   else // no Obstacle
   {
-    myUpFade(4);
-    myDownFade(4);
+    Serial.println("clear");
   }
   delay(200);
-}
-
-void myBlink(int t) {
-  digitalWrite(9, HIGH);
-  delay(t);
-  digitalWrite(9, LOW);
-  delay(t);
-}
-
-void myUpFade(int s) {
-  int i=0;
-  while (i < 256) {
-    analogWrite(9, i);
-    i = i + s;
-    delay(10);
-  }
-}
-
-void myDownFade(int s) {
-  int i=255;
-  while (i > 0) {
-    analogWrite(9, i);
-    i = i - s;
-    delay(10);
-  }
 }
