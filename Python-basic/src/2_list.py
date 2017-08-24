219bf86699edd7a8d80be78fc8e000ba01c4b6a5
yoshihiroo <yoshihiroo@hotmail.com>
2017-08-24 11:37:34 -0700

    Create 2_list.py

---
+++ Python-basic/src/2_list.py
@@ -0,0 +1,22 @@
+#リスト
+
+a = [1, 3, 5]
+b = [10, 100, 1000]
+c = a + b
+
+print(a)
+print(a[0])
+print(a[1])
+print(a[2])
+
+print(b)
+print(b[2])
+print(b[-1])
+
+print(c)
+print(c[0], c[2], c[5])
+print(len(c))
+
+'''
+リストの要素やインデックスをいろいろ変えてみる
+'''
