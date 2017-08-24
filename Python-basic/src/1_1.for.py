42bd7c3f207e992c94aa8c24f6bda7168106d753
yoshihiroo <yoshihiroo@hotmail.com>
2017-08-24 11:33:51 -0700

    Create 1_1.for.py

---
+++ Python-basic/src/1_1.for.py
@@ -0,0 +1,22 @@
+#for文による繰り返し
+
+for i in range(10):
+    print("hello")
+
+'''
+print文の中身を自由に書き換えてみる
+'''
+
+
+#繰り返しの中で、変数を使った計算をする
+
+for i in range(5):
+    print(i)
+
+for i in range(5):
+    a = 10 * i
+    print(a)
+
+'''
+掛け算以外の演算も試してみる
+'''
