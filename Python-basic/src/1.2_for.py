6b3eb91fdbe842de10561af5356cb9715801bd43
yoshihiroo <yoshihiroo@hotmail.com>
2017-08-24 11:35:07 -0700

    Create 1.2_for.py

---
+++ Python-basic/src/1.2_for.py
@@ -0,0 +1,17 @@
+#繰り返しの中で繰り返しを入れ子にする
+
+for i in range(0, 3):
+    for j in range(0, 3):
+        print("i =", i, ", j =", j)
+
+
+#九九の掛け算を計算し表示させる
+
+for i in range(1, 10):
+    for j in range(1, 10):
+        print(i*j, end=" ")
+    print("")
+
+'''
+普段日本人はなじみのない10~16x10~16の掛け算表を作るには？
+'''
