742fa796da1738a6db9e1b087a91ec0ab99106fd
yoshihiroo <yoshihiroo@hotmail.com>
2017-08-24 11:38:10 -0700

    Create 3.1_if.py

---
+++ Python-basic/src/3.1_if.py
@@ -0,0 +1,12 @@
+#リスト
+
+a = [-10, -3, 0, 5, 10]
+print(a)
+
+for i in a:
+    if i > 0:
+        print(i, "is lager than zero")
+    elif i == 0:
+        print(i, "is zero")
+    else:
+        print(i, "is less than zero")
