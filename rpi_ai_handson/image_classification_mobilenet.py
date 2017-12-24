b96908829edc83b7771c2666f1667a1b845e1c55
Yoshihiro Ochi <9767564+yoshihiroo@users.noreply.github.com>
2017-12-24 16:00:46 +0900

    Update image_classification_mobilenet.py

--- rpi_ai_handson/image_classification_mobilenet.py
+++ rpi_ai_handson/image_classification_mobilenet.py
@@ -6,9 +6,11 @@ import sys, os, time, subprocess, pickle
 
 model = MobileNet(weights='imagenet')
 devnull = open('os.devnull', 'w')
+ipaddr = subprocess.check_output(["hostname", "-I"]).decode("utf-8").strip()
+commd = "http://"+ipaddr+":8080/?action=snapshot"
 
 while True:
-    subprocess.run(["wget", "-O", "photo.jpg", "http://192.168.1.46:8080/?action=snapshot"], stdout=devnull, stderr=subprocess.STDOUT)
+    subprocess.run(["wget", "-O", "photo.jpg", commd], stdout=devnull, stderr=subprocess.STDOUT)
 
     img_path = 'photo.jpg'
     img = image.load_img(img_path, target_size=(224, 224))
